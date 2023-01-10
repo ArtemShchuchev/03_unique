9f214a11e98b3495ec05f764a43c4c8f3279aa3b
ArtemShchuchev <shuaa@rambler.ru>
2023-01-11 01:56:59 +0700

    add class unique_ptr

--- Unique/Unique.hpp
+++ Unique/Unique.hpp
@@ -1,9 +1,52 @@
 #pragma once
 
 template<class T>
-class Unique
+class unique_ptr
 {
 private:
+	T* ptr = nullptr;
 public:
+	unique_ptr(T*);		// Конструктор, принимающий сырой указатель
+	unique_ptr(const unique_ptr &) = delete;	// конструктор копирования
+	~unique_ptr();		// деструктор
+
+	unique_ptr& operator= (const unique_ptr&) = delete;	// оператор присваивания
+	T operator* ();		// оператор разименования
+	T* my_release();	// освобождает владение и возвращает сырой указатель.
 };
 
+
+
+template<class T>
+inline unique_ptr<T>::unique_ptr(T* rawPtr) : ptr(rawPtr)
+{
+}
+
+template<class T>
+inline unique_ptr<T>::~unique_ptr()
+{
+	std::cout << "destructor\n";
+	if (ptr)
+	{
+		std::cout << "delete...\n";
+		delete ptr;
+	}
+}
+
+template<class T>
+inline T unique_ptr<T>::operator*()
+{
+	if (!ptr) throw std::runtime_error("Объект не существует!");
+	return *ptr;
+}
+
+template<class T>
+inline T* unique_ptr<T>::my_release()
+{
+	auto temp = new T;
+	*temp = *ptr;
+	delete ptr;
+	ptr = nullptr;
+
+	return temp;
+}
--- main.cpp
+++ main.cpp
@@ -17,9 +17,41 @@
 5. Деструктор.
 */
 
+struct S
+{
+    S() : a(0), b(0) {}
+    S(int _a, double _b) : a(_a), b(_b) {}
+    friend std::ostream& operator<< (std::ostream& out, const S& s)
+    {
+        return out << "\na = " << s.a << "\nb = " << s.b << "\n\n";
+    }
+
+    int a;
+    double b;
+};
+
 int main()
 {
     printHeader("Задача 3*. std::unique_ptr своими руками");   // заголовок
+
+    auto rawPtr = new S(5, 16.58);
+    std::cout << "rawPtr: " << *rawPtr << "\n";
+
+    unique_ptr<S>unicPtr(new S(3, 2.3));
+    std::cout << "unicPtr: " << *unicPtr << "\n";
+
+    rawPtr = unicPtr.my_release();
+    std::cout << "rawPtr: " << *rawPtr << "\n";
+    
+    try
+    {
+        // объект unicPtr "пустой"
+        std::cout << "unicPtr: " << *unicPtr << "\n";
+    }
+    catch (const std::runtime_error& err)
+    {
+        std::cout << "Ошибка! " << err.what() << "\n";
+    }
 
     return 0;
 }
